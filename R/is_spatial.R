93fa8b0939244336cc8f3f4f4ad59e44a5847192
Sarah Rockhill <ppk8@cdc.gov>
2025-01-08 14:15:25 -0500

    add function to verify that input data are spatial

---
+++ R/is_spatial.R
@@ -0,0 +1,9 @@
+#' Function to verify that object is spatial data.
+#'
+#' @param x
+#'
+#' @return Returns True if object is spatial and False otherwise.
+#' @export
+is_spatial <- function(x){
+  if (class(x)[1] == 'sf' | grepl("spatial", class(x)[1], ignore.case = TRUE)) TRUE else FALSE
+}
