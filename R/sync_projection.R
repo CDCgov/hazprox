6ce468244f40c32b692a4d70e8260c789e8ae512
Sarah Rockhill <ppk8@cdc.gov>
2025-01-08 14:15:50 -0500

    better handling of dependencies

--- R/sync_projection.R
+++ R/sync_projection.R
@@ -1,9 +1,11 @@
-library(sf)
-library(dplyr)
-library(glue)
-#-------------------------------------------------------------------------------
-#Function to ensure polygon and points are in same projected coordinate system
-#-------------------------------------------------------------------------------
+#' Ensures that spatial data are in same projected coordinate system.
+#'
+#' @param df1 A spatial data frame
+#' @param df2 A spatial data frame
+#' @return A list object containing both spatial data frames projected in the same CRS.
+#'
+#' @importFrom glue glue
+#'
 #' @export
 sync_projection<-function(df1, df2){
 
@@ -11,28 +13,28 @@ sync_projection<-function(df1, df2){
   df2_name <- deparse(substitute(df2))
 
   #Check if inputs in geographic CRS
-  if (st_is_longlat(df1) & st_is_longlat(df2)) {
+  if (sf::st_is_longlat(df1) & sf::st_is_longlat(df2)) {
     stop("Error: The data are not in a projected CRS.\n")
   }
 
   #Check if inputs projected in the same CRS
-  else if (!st_is_longlat(df1) & !st_is_longlat(df2) &
-           st_crs(df1)!=st_crs(df2)) {
-    stop("Error: The data are not in the same projected CRS.\nTransform your data before proceeding.\n")
+  else if (!sf::st_is_longlat(df1) & !sf::st_is_longlat(df2) &
+           sf::st_crs(df1)!=sf::st_crs(df2)) {
+    stop("Error: Input data are not in the same projected CRS.\nTransform your data before proceeding.\n")
   }
 
   #if a single input is projected, project other input in same CRS
-  else if (!st_is_longlat(df1) & st_is_longlat(df2)) {
+  else if (!sf::st_is_longlat(df1) & sf::st_is_longlat(df2)) {
     message(glue("{df2_name} is not in a projected CRS.
-                 Projecting {df2_name} into ({st_crs(df1)$proj4string}).\n"))
-    df2 <- df2 |> st_transform(crs=st_crs(df1))
+                 Projecting {df2_name} into ({sf::st_crs(df1)$proj4string}).\n"))
+    df2 <- df2 |> sf::st_transform(crs=sf::st_crs(df1))
   }
 
   #if a single input is projected, project other input in same CRS
-  else if (st_is_longlat(df1) & !st_is_longlat(df2)) {
+  else if (sf::st_is_longlat(df1) & !sf::st_is_longlat(df2)) {
     message(glue("{df1_name} is not in a projected CRS.
-                 Projecting {df1_name} into ({st_crs(df2)$proj4string}).\n"))
-    df1 <- df1 |> st_transform(crs=st_crs(df2))
+                 Projecting {df1_name} into ({sf::st_crs(df2)$proj4string}).\n"))
+    df1 <- df1 |> sf::st_transform(crs=sf::st_crs(df2))
   }
 
   else {
