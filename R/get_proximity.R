5c96feafd950f101193df8e5d3115197619a0bf6
Sarah Rockhill <ppk8@cdc.gov>
2025-04-17 10:14:17 -0400

    add option to apply weights to hazards

--- R/get_proximity.R
+++ R/get_proximity.R
@@ -14,12 +14,25 @@
 #' @param to_points A spatial points layer with class sf or sfc
 #' representing the environmental hazard of interest.
 #' @param tolerance The maximum search distance for environmental hazards (optional).
-#' @param units The units of the tolerance value (e.g., "m" ,  "km", "ft", "yd",  "fathom", "mi", "naut_mi", "au")
+#' @param units The units of the tolerance value (e.g., "m" ,  "km", "ft", "yd",  "fathom", "mi", "naut_mi", "au"),
+#' if tolerance is specified.
+#' @param weights An optional vector with the same length as to_points that applies a weight to each hazard.
+#'
 #'
 #' @importFrom rlang .data
 #'
 #' @export
-get_proximity<-function(from_poly, to_points, tolerance=NULL, units = 'km'){
+get_proximity<-function(from_poly, to_points, tolerance=NULL, units='km', weights=NULL){
+
+  #Verify length weights matches points (if provided)
+  if(missing(weights)) {
+    weights =  rep(1, nrow(to_points))
+  }
+
+  if(length(weights) != nrow(to_points))
+    stop("'to_points' and 'weights' must have the same length")
+
+  weights = data.frame(wt = weights, point = paste0('X', 1:nrow(to_points)))
 
   #Ensure polygon and points are sf objects with same CRS
   dflist <- sync_projection(from_poly, to_points)
@@ -66,6 +79,9 @@ get_proximity<-function(from_poly, to_points, tolerance=NULL, units = 'km'){
                                              .data$Distance * 0.90,
                                              .data$Distance))
 
+  #Merge user-provided weights
+  distances <- distances |> dplyr::left_join(weights, dplyr::join_by(point))
+
   #If tolerance is not provided, set to maximum distance. Otherwise convert to km
   if (missing(tolerance)) {
     tolerance <- max(distances$Distance, na.rm=T)
@@ -77,8 +93,8 @@ get_proximity<-function(from_poly, to_points, tolerance=NULL, units = 'km'){
   distances<- distances |>
     dplyr::group_by(.data$ID) |>
     dplyr::summarise(Proximity = ifelse(sum(.data$Distance < tolerance)==0,
-                                    1/min(.data$Distance),
-                                    sum(1/.data$CorrectedDistance[.data$Distance<tolerance])))
+                                    (1/min(.data$Distance))*.data$wt[which.min(.data$Distance)],
+                                    sum((1/.data$CorrectedDistance[.data$Distance<tolerance])*.data$wt[.data$Distance<tolerance])))
 
 
   return(distances$Proximity)
