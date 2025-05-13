4d5e40410eab9f03383b3e4574fc059bc5060c60
Sarah Rockhill <ppk8@cdc.gov>
2025-05-13 16:39:42 -0400

    refactor code and doc

--- R/avg_proximity.R
+++ R/avg_proximity.R
@@ -1,40 +1,35 @@
 #' @title avg_proximity
 #'
 #' @description
-#' Function to calculate average proximity to point-source
-#' environmental hazards within a specified area. The function
-#' will calculate a population weighted average if the user
-#' supplies a vector of population counts or weights.
+#' Function to calculate average proximity to environmental hazards within a specified area.
+#' The function will calculate a population weighted average if the user supplies a vector
+#' of population counts or weights.
 
-#' @param from_poly A spatial polygon layer
-#' @param to_points A spatial points layer that represents the
-#' environmental hazards.
-#' @param tolerance The maximum search distance for environmental hazards.
-#' @param group Name of variable in the from_poly layer that defines the group means
-#' are calculated for.
-#' @param pop_weights An optional vector of population weights if
-#' weighted-averages are desired.
+#' @param from A spatial polygon layer with class sf or sfc. Proximity is calculated from
+#' the geometric center of each polygon to each hazard within the search tolerance.
+#' @param to A spatial polygon, point, or linestring layer with class sf or sfc
+#' representing the environmental hazard(s) of interest.
+#' @param ... Additional parameters
+#' @param group Name of grouping variable in the `from` layer. Average proximity is calculated
+#' among polygons nested in the group.
+#' @param pop_weights An optional vector of population weights if weighted-averages are desired.
 #'
 #' @importFrom rlang .data
 #'
 #' @export
-avg_proximity <- function(from_poly, to_points, tolerance, group, pop_weights=NULL) {
+avg_proximity <- function(from, to, ...,  group, pop_weights=NULL) {
 
   #Verify that weights are provided for each unit, if any
-  if(!is.null(pop_weights) && nrow(from_poly) != length(pop_weights)) {
-    stop('Error: from_poly and pop_weights must have same length.')
+  if(!is.null(pop_weights) && nrow(from) != length(pop_weights)) {
+    stop('Error: `from` and `pop_weights` must have same length.')
   }
 
   #Calculate proximity for each unit of from_poly
-  from_poly$unit_prox <- get_proximity(
-        from = from_poly,
-        to = to_points,
-        tolerance = tolerance
-        )
+  from$unit_prox <- get_proximity(from, to, ...)
 
   #Calculated weighted average, ignore missing values
   if(!is.null(pop_weights)) {
-    result <- from_poly |>
+    result <- from |>
       cbind(pop_weights = pop_weights) |>
       dplyr::group_by(!!!rlang::syms(group)) |>
       dplyr::summarise(avg_prox = sum(.data$pop_weights  * .data$unit_prox, na.rm=T) /
@@ -42,7 +37,7 @@ avg_proximity <- function(from_poly, to_points, tolerance, group, pop_weights=NU
   }
 
   if(is.null(pop_weights)) {
-     result <- from_poly|>
+     result <- from|>
       dplyr::group_by(!!!rlang::syms(group)) |>
       dplyr::summarise(avg_prox = mean(.data$unit_prox, na.rm=T))
   }
