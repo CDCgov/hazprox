37e90b879568d50222444f8ed52e126038e20e53
Sarah Rockhill <ppk8@cdc.gov>
2025-06-16 15:47:48 -0400

    move data for unit tests into tests/testthat/testdata

--- tests/testthat/test-get_proximity.R
+++ tests/testthat/test-get_proximity.R
@@ -1,19 +1,19 @@
 test_that("basic get_proximity works", {
-  ga <- ga[1:10,]
-  pts <- npls[1:10, ]
-  wts <- runif(10, 0, 10)
+  poly <- readRDS(test_path("testdata/poly.rds"))
+  pts <- readRDS(test_path("testdata/pts.rds"))
+  wts <- runif(nrow(pts), 0, 10)
   coords <- matrix(c(0, 0, 1, 0, 1, 1, 0, 1, 0, 0), ncol = 2, byrow = TRUE)
   m <- sf::st_polygon(list(coords))
 
-  expect_silent(get_proximity(ga, pts))
-  expect_silent(get_proximity(ga$geometry, pts))
-  expect_silent(get_proximity(ga, ga$geometry))
-  expect_silent(get_proximity(ga, pts, tolerance = 100))
-  expect_silent(get_proximity(ga, pts, tolerance = 100, units = 'km'))
-  expect_silent(get_proximity(ga, pts, tolerance = 100, units = 'km', weights = wts))
-  expect_length(get_proximity(ga, pts), 10)
+  expect_silent(get_proximity(poly, pts))
+  expect_silent(get_proximity(poly$geometry, pts))
+  expect_silent(get_proximity(poly, poly$geometry))
+  expect_silent(get_proximity(poly, pts, tolerance = 100))
+  expect_silent(get_proximity(poly, pts, tolerance = 100, units = 'km'))
+  expect_silent(get_proximity(poly, pts, tolerance = 100, units = 'km', weights = wts))
+  expect_length(get_proximity(poly, pts), nrow(poly))
 
-  expect_error(get_proximity(ga, pts, weights = wts[-1]), "`to` and `weights` must have the same length")
+  expect_error(get_proximity(poly, pts, weights = wts[-1]), "`to` and `weights` must have the same length")
   expect_error(get_proximity(m, pts), '`from` must be a spatial polygon')
-  expect_error(get_proximity(pts, ga), '`from` must be a spatial polygon')
+  expect_error(get_proximity(pts, poly), '`from` must be a spatial polygon')
 })
