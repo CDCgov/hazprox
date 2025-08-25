9145369f782a63c7b8a2a4a15bd212a0953a1e49
Rockhill <ppk8@cdc.gov>
2025-08-25 15:26:26 -0400

    add package level documentation

---
+++ R/hazprox-package.R
@@ -0,0 +1,12 @@
+#' @description
+#' A package for measuring cumulative proximity to hazards for geographic areas.
+#' To learn more about hazprox, start with the vignettes:
+#'
+#' `browseVignettes(package = "hazprox")`
+#'
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
